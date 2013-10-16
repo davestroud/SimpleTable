db909ee530918d42f03145b8fdbe63f0d074d722
David Stroud <david@greymatter-ag.com>
2013-10-16 17:28:17 -0400

    first commit

---
+++ Simple Table/BIDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BIDAppDelegate.h
+//  Simple Table
+//
+//  Created by DAVID STROUD on 10/16/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Simple Table/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Simple Table
+//
+//  Created by DAVID STROUD on 10/16/13.
+//  Copyright (c) 2013 Grey Matter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
